d38da8a89587e86b793da4845cdeb2ca7f45c91c
itsnotgoodname <gurnaindeol@gmail.com>
2020-02-15 18:16:48 -0800

    Add get_around and change move code

--- snake.py
+++ snake.py
@@ -5,6 +5,7 @@ from constants import *
 
 class Snake:
     MOVE_MATRIX = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+    MOVE_MATRIX_LEN = len(MOVE_MATRIX)
 
     def __init__(self, x, y):
         self.body = []
@@ -41,14 +42,10 @@ class Snake:
         board[y][x] = SNAKE_HEAD_NUM
 
     def move_left(self):
-        self.move_ind -= 1
-        if self.move_ind < 0:
-            self.move_ind = 3
+        self.move_ind = (self.move_ind - 1) % self.MOVE_MATRIX_LEN
 
     def move_right(self):
-        self.move_ind += 1
-        if self.move_ind > 3:
-            self.move_ind = 0
+        self.move_ind = (self.move_ind + 1) % self.MOVE_MATRIX_LEN
 
     def is_dead(self):
         if (self.x, self.y) in self.body or self.x >= NUM_X_GRID or self.x < 0 or self.y >= NUM_Y_GRID or self.y < 0:
@@ -60,3 +57,17 @@ class Snake:
             x, y = b
             pygame.draw.rect(win, [0, 0, 0], (x * X_GRID_SIZE, y * Y_GRID_SIZE, X_GRID_SIZE, Y_GRID_SIZE))
         pygame.draw.rect(win, [128,128, 128], (self.x * X_GRID_SIZE, self.y * Y_GRID_SIZE, X_GRID_SIZE, Y_GRID_SIZE))
+
+    def get_snake_around(self):
+        ret = []
+        for xi, yi in self.MOVE_MATRIX:
+            if self.x + xi < 0 or self.x + xi >= NUM_X_GRID or self.y + yi < 0 or self.y + yi >= NUM_Y_GRID or (self.x+xi, self.y+yi) in self.body:
+                ret.append(WALL_NUM)
+            else:
+                ret.append(NOTHING_NUM)
+
+        left = ret[(self.move_ind - 1) % self.MOVE_MATRIX_LEN]
+        right = ret[(self.move_ind + 1) % self.MOVE_MATRIX_LEN]
+        straight = ret[self.move_ind]
+
+        return (left, straight, right)
